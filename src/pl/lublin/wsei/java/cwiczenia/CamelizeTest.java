316640b5594989dcff9757692685f1a62ea4ca7e
kamil_barwinski <kamil.barwinski1bt1@gmail.com>
2022-06-03 11:24:37 +0200

    Zadanie B

--- src/pl/lublin/wsei/java/cwiczenia/CamelizeTest.java
+++ src/pl/lublin/wsei/java/cwiczenia/CamelizeTest.java
@@ -1,16 +1,12 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-
 import pl.lublin.wsei.java.cwiczenia.mylib.StringFun;
 
-public class Main {
 
+public class CamelizeTest {
     public static void main(String[] args) {
         StringFun sf = new StringFun();
-        String txt = "Los Angeles";
-                System.out.printf(StringFun.translit(txt));
-	// write your code here
+        String txt = "Taka Zabawna funkcja";
+        System.out.printf(StringFun.camelize(txt));
     }
 }
-
-
--- src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/StringFun.java
@@ -3,20 +3,39 @@ package pl.lublin.wsei.java.cwiczenia.mylib;
 
 import java.util.Locale;
 
+import static java.lang.String.valueOf;
+
 public class StringFun {
 
-    public static  String translit(String arg) {
+    public static String translit(String arg) {
         var result = new StringBuilder();
+
         for (int index = 0; index < arg.length(); index++) {
-            var orginalLetter = arg.charAt(index);
-            var letter = String.valueOf(orginalLetter);
-            if(Character.isUpperCase(orginalLetter) == true) {
+            var originalLetter = arg.charAt(index);
+            var letter = valueOf(originalLetter);
+            if (Character.isUpperCase(originalLetter) == true) {
                 var l1 = letter.toLowerCase(Locale.ROOT);
                 result.append(l1);
             } else {
                 var l2 = letter.toUpperCase(Locale.ROOT);
                 result.append(l2);
             }
+        }
+        return result.toString();
+    }
+
+    public static String camelize(String arg) {
+        String[] words = arg.split("[\\W_]+");
+
+        var result = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (i == 0) {
+                word = word.isEmpty() ? word : word.toLowerCase();
+            } else {
+                word = word.isEmpty() ? word : Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+            }
+            result.append(word);
         }
         return result.toString();
     }
